a872f3047c5ae1ac1df34c8e84e36e1b3092d11b
kacem <kacemmeherzi52@gmail.com>
2025-01-26 17:39:32 +0100

    add main.js to initialize Electron app and create main window

---
+++ main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // Load the Angular app from the dist folder (after build)
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, 'dist/electron-angular-app/index.html'),
+      protocol: 'file:',
+      slashes: true,
+    })
+  );
+
+  // Open dev tools
+  win.webContents.openDevTools();
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (win === null) createWindow();
+});
