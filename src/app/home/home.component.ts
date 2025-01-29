53f376caa586c319b5f26c7c1d5c649865a58f8e
kacem <kacemmeherzi52@gmail.com>
2025-01-29 15:02:41 +0100

    implement file selection in HomeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ElectronService } from '../services/electronservices.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,29 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  selectedFile: File | null = null;
 
+  constructor(private electronService: ElectronService) {}
+
+  ngOnInit(): void {
+    if (!window.electronAPI) {
+      console.error('Electron API is not available.');
+    }
+  }
+  onFileSelected(): void {
+    this.electronService.selectFile().then((file : string)=>{
+      console.log(file);
+      
+    });
+  }
 }
+/*
+  readPdf(file: File): void {
+    const reader = new FileReader();
+    reader.onload = () => {
+      const pdfData = reader.result;
+      console.log('PDF content:', pdfData);
+    };
+    reader.readAsArrayBuffer(file);
+  }*/
